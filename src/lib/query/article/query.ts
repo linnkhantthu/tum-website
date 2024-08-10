4e9e6ec74f3fdeaaadce0463a3a69eb59ea93756
linnkhantthu <linnkhantthu1999mdy@gmail.com>
2024-08-10 22:05:50 +0630

    added image deletion to article DELETE route

--- src/lib/query/article/query.ts
+++ src/lib/query/article/query.ts
@@ -1,4 +1,7 @@
 import prisma from "@/db";
+import { storage } from "@/lib/firebase";
+import { Article } from "@/lib/models";
+import { deleteObject, ref } from "firebase/storage";
 
 /**
  * Get User by Email
@@ -126,8 +129,46 @@ export async function deletedArticleById(id: string) {
   try {
     article = await prisma.article.delete({ where: { id: id } });
     message = "Deleted article successfully.";
+    const _article = article as Article;
+    const image: any = _article.content.blocks.filter(
+      (value) => value.type === "image"
+    );
+    const url: string = image.data.file.url;
+    const filename = url.split("%2F")[1].split("?")[0];
+    const storageRef = ref(storage, `images/${filename}`);
+    await deleteObject(storageRef);
   } catch (error) {
     message = error;
   }
   return { article, message };
 }
+
+const data = {
+  time: 1723193301012,
+  blocks: [
+    {
+      id: "U0X__4rBEq",
+      data: {
+        file: {
+          url: "https://firebasestorage.googleapis.com/v0/b/tum-webstorage.appspot.com/o/images%2F9821970645515ab3cc1346abcbd8dfa0.jpg?alt=media&token=71edaa8f-ea45-4f8e-a000-1cafee853919",
+        },
+        caption: "Crypto report II",
+        stretched: false,
+        withBorder: false,
+        withBackground: false,
+      },
+      type: "image",
+    },
+    {
+      id: "QoxJxf97HA",
+      data: { text: "Crypto lab report II", level: 1 },
+      type: "header",
+    },
+    {
+      id: "2CX1Ve2nOz",
+      data: { text: "This is cloud lab report II about crypto" },
+      type: "paragraph",
+    },
+  ],
+  version: "2.30.2",
+};
