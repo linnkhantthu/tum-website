749bc988f41b074db326acef6a715223d48f453a
linnkhantthu <linnkhantthu1999mdy@gmail.com>
2024-09-09 11:38:21 +0630

    added main sitemap

---
+++ src/app/articles/sitemap.ts
@@ -0,0 +1,36 @@
+import { Article } from "@/lib/models";
+import type { MetadataRoute } from "next";
+
+export async function generateSitemaps() {
+  // Get Total Number of articles and divide by 50000
+  return [{ id: 0 }];
+}
+
+export default async function sitemap({
+  id,
+}: {
+  id: string;
+}): Promise<MetadataRoute.Sitemap> {
+  // Google's limit is 50,000 URLs per sitemap
+  const res = await fetch(
+    `${process.env.BASE_URL}/api/articles?isPublished=${true}&skip=${id}&take=${
+      id + 50000
+    }`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  if (res.ok) {
+    const { articles, message }: { articles: Article[]; message: string } =
+      await res.json();
+    return articles.map((article) => ({
+      url: `${process.env.BASE_URL}/articles/${article.id}/${article.slug}`,
+      lastModified: new Date(),
+    }));
+  } else {
+    return [];
+  }
+}
